1f2f4371a40678e949121694fb8ceb991bde2c2f
TAKOS <109962559+FncTAKOS@users.noreply.github.com>
2023-12-04 14:11:47 +0100

    Delete december_day4.py

--- december_day4.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def largestGoodInteger(self, num):
-        res=0
-        for i in range(len(num)-2):
-            if(num[i]==num[i+1]==num[i+2]):
-                res=max(res,num[i])
-        return "" if res==0 else str(res)*3
